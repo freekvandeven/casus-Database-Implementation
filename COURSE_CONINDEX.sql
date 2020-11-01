a35c9ede75fc356f8b27d1d5339c6765decf99e3
Freek van de Ven <freek@freekvandeven.nl>
2020-11-01 20:26:03 +0100

    add security tests and indexes

--- COURSE_CONINDEX.sql
+++ COURSE_CONINDEX.sql
@@ -1,9 +1,45 @@
 -- fix for bad stored procedure
 
-
+BEGIN TRANSACTION;
+SAVE TRANSACTION @savepoint;
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE
+----------------------------------------
+IF(EXISTS(SELECT 1 FROM emp e left join term t on t.empno=e.empno WHERE e.empno = @empno and isnull(t.leftcomp,'1753-1-1') < e.hired AND JOB = 'ADMIN')
+			AND (SELECT COUNT(e.empno) FROM emp e left join term t on e.empno = t.empno WHERE deptno = (SELECT deptno FROM emp WHERE empno = @empno) AND job = 'ADMIN' and isnull(t.leftcomp,'1753-1-1') < e.hired GROUP BY deptno)<2
+			AND EXISTS (SELECT 1 FROM emp e left join term t on e.empno = t.empno WHERE deptno = (SELECT deptno FROM emp WHERE empno = @empno) AND (job = 'MANAGER' OR job='PRESIDENT') and isnull(t.leftcomp,'1753-1-1') < e.hired))
+		BEGIN
+			;THROW 50000, 'Every department that employs a manager or president must have at least one administrator', 1
+		END
+		IF(EXISTS(SELECT 1 FROM term where empno = @empno))
+		BEGIN
+			UPDATE term set empno = @empno, leftcomp = getdate(), comment = @comment
+		END
+		ELSE
+		BEGIN
+			INSERT INTO term values(@empno,getdate(),@comment)
+		END
+----------------------------------------
+COMMIT TRANSACTION;
 
 
 -- index 1
+BEGIN TRAN
+DBBC FREEPROCCACHE
+DBCC DROPCLEANBUFFERS
+EXEC SP_InsertEmployee 1664, Jaap, 'ADMIN', '2001-09-11', '2019-09-12', 2,2400,'MO324',10
+ROLLBACK TRAN
 
+
+CREATE NONCLUSTERED INDEX IX_EMP_DEPTNO_JOB_HIRED
+ON EMP (DEPTNO,JOB,HIRED)
 
 -- index 2
+BEGIN TRAN
+DBBC FREEPROCCACHE
+DBCC DROPCLEANBUFFERS
+INSERT INTO OFFR VALUES
+('AM4DP','1997-10-06','CONF',6,1017,'SAN FRANCISCO')
+ROLLBACK TRAN
+
+CREATE NONCLUSTERED INDEX IX_OFFR_TRAINER
+ON OFFR (TRAINER)
--- COURSE_SECURITY.sql
+++ COURSE_SECURITY.sql
@@ -3,38 +3,54 @@ F - SECURITY
 ------------------------------------------------------------------------------------------------*/
 
 
--- create employee user
-GO
-CREATE LOGIN [Employee] WITH PASSWORD=N'klootviool', DEFAULT_DATABASE=[COURSE]
-CREATE USER [Employee] FOR LOGIN [Employee]
--- make this a role instead?
+-- create employee role
+CREATE ROLE Employee
 
+CREATE LOGIN [Employee1] WITH PASSWORD=N'qwerty', DEFAULT_DATABASE=[COURSE]
+CREATE USER [Employee1] FOR LOGIN [Employee1]
+-- assign new employee to the employee role
+ALTER ROLE Employee ADD MEMBER [Employee1]
 
 -- give permission for data
-GRANT EXECUTE ON dbo.reg TO Employee
+GRANT CONTROL ON dbo.reg TO Employee
 GRANT SELECT ON dbo.emp TO Employee
 GRANT SELECT ON dbo.offr TO Employee
 
 
--- create reporting user
-CREATE LOGIN [Reporting] WITH PASSWORD=N'kutverslag', DEFAULT_DATABASE=[COURSE]
-CREATE USER [Reporting] FOR LOGIN [Reporting]
+-- create reporting role
+CREATE ROLE Reporting
+
+CREATE LOGIN [Employee2] WITH PASSWORD=N'qwerty', DEFAULT_DATABASE=[COURSE]
+CREATE USER [Employee2] FOR LOGIN [Employee2]
+-- assign new employee to the reporting role
+ALTER ROLE Reporting ADD MEMBER [Employee2]
 
 GRANT SELECT ON SCHEMA :: [dbo] TO Reporting
 
 
 -- testen van security policy
-
--- test empolyee account
-EXECUTE AS USER = 'Employee'
+-- test employee1 account
+EXECUTE AS USER = 'Employee1'
 
 -- test full access Reg table
+SELECT * FROM reg
+
+BEGIN TRANSACTION
+UPDATE reg
+SET eval = 5 WHERE stud = 1000
+ROLLBACK TRANSACTION
 
 -- test read access emp & offr table
+SELECT * FROM emp
+SELECT * FROM offr
 
 
+REVERT --switch back to previous user
 
 -- test reporting account
-EXECUTE AS USER = 'Reporting'
+EXECUTE AS USER = 'Employee2'
+-- test full read access on all data
+SELECT * FROM grd
 
+--test no access to insert
 INSERT INTO dbo.grd VALUES(12,12000,17000,6000)
